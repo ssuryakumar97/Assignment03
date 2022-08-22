593c00c15c612a1e18a29745e9d7e7e50a7b9047
ssuryakumar97 <107339304+ssuryakumar97@users.noreply.github.com>
2022-08-22 18:19:54 +0530

    Add files via upload

---
+++ scriptresume.js
@@ -0,0 +1,11 @@
+let Resume = {
+    "Name" : " Surya Kumar S",
+    "DOB" : "26/07/1997",
+    "Email" : "ssuryakumar97@gmail.com",
+    "objective" : "To work in a healthy, innovative and challenging environment extracting the best out of me, which is conducive to learn and grow at professional as well as personal level thereby directing my future endeavors as an asset to the organization.",
+    "Academic qualification" : "B.E Mechanical Engineering",
+    "University" : "Anna University",
+    "Experience" : " 3 years",
+    "Projects Undertaken" : "Performance analysis of vapour compression refrigeration system using nanofluids.",
+}
+console.log(Resume);
